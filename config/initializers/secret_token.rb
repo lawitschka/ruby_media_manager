3f6cb81e2b05979b1332670f2121f6d4306676d9
Moritz Lawitschka <moritz@urbanvention.com>
2013-04-08 00:59:25 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RMM::Application.config.secret_token = 'f6a85f485a70ab48ef853158540f84632c694c696f531369bd37c459e8a98f3b7d3e4e39f158a4c8e65795dd695d45686267f07796867ec6902733a95bd5408b'
