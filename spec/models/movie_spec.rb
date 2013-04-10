479b53c565b3865d6a71b12451b0daaeb0b2f7c8
Moritz Lawitschka <moritz@urbanvention.com>
2013-04-10 23:00:36 +0200

    Movie specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:movie) { Movie.make! }
+
+
+  #########################
+  # Attribute existence
+  #########################
+
+  it { should respond_to :imdb_id }
+  it { should respond_to :tmdb_id }
+  it { should respond_to :title }
+  it { should respond_to :original_title }
+  it { should respond_to :year }
+  it { should respond_to :release_date }
+  it { should respond_to :rating }
+  it { should respond_to :votes }
+  it { should respond_to :certification }
+  it { should respond_to :tagline }
+  it { should respond_to :outline }
+  it { should respond_to :plot }
+  it { should respond_to :runtime }
+
+
+  #########################
+  # Validations
+  #########################
+
+  it { should validate_presence_of :title }
+
 end
