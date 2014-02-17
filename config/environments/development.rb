e94b8732172be3e38a923e03306594a1be0f93d9
Moritz Lawitschka <moritz@urbanvention.com>
2014-02-17 21:46:22 +0100

    Configure which Ember.js version to use in each environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ RubyMediaManager::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Ember environment
+  config.ember.variant = :development
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ RubyMediaManager::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Ember environment
+  config.ember.variant = :development
 end
